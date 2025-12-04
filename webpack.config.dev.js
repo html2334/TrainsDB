6d78c29f6da68b1bcde10fc5836fa9425fa56778
html2334 <bailey.nysen@education.nsw.gov.au>
2025-12-04 13:40:46 +1100

    Initial commit

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,13 +1,16 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
+//const { merge } = require('webpack-merge');
+//const common = require('./webpack.common.js');
 
-module.exports = merge(common, {
+module.exports = /*merge(common, */{
+  entry: {
+    app: '/public_html/index.js'
+  },
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
     liveReload: true,
     hot: true,
-    open: true,
-    static: ['./'],
+    open: false,
+    static: ['./public_html/'],
   },
-});
+}//);
