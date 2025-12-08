f831964d573ec84e9b0530cd950674e90f33502a
html2334 <bailey.nysen@education.nsw.gov.au>
2025-12-08 11:23:18 +1100

    Switch to better-sqlite3

--- backend/backend.js
+++ backend/backend.js
@@ -1,19 +1,22 @@
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('db/a.sqlite');
+const db = require('better-sqlite3')('foobar.db', {});
+//const db = new sqlite3.Database('db/a.sqlite');
 
-db.serialize(() => {
-  db.run("DROP TABLE lorem")
-  db.run("CREATE TABLE lorem (info TEXT)");
+db.prepare("DROP TABLE lorem").run()
+db.prepare("CREATE TABLE lorem (info TEXT)").run()
 
-  const stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-  for (let i = 0; i < 10; i++) {
-    stmt.run("Ipsum " + i);
-  }
-  stmt.finalize();
+const stmt = db.prepare("INSERT INTO lorem VALUES (?)")
+for (let i = 0; i < 10; i++) {
+  stmt.run("Ipsum " + i)
+}
 
-  db.each("SELECT rowid AS id, info FROM lorem", (err, row) => {
-    console.log(row.id + ": " + row.info);
-  });
-});
+/*db.each("SELECT rowid AS id, info FROM lorem", (err, row) => {
+  console.log(row.id + ": " + row.info);
+});*/
+//let log = db.prepare("SELECT rowid AS id, info FROM lorem").all()
+for (const x of db.prepare("SELECT rowid AS id, info FROM lorem").all()) {
+  console.log(x.id + ": " + x.info)
+}
 
+
+db.prepare("DROP TABLE lorem").run()
 db.close();
