72f5117662212fc35f95ec7821c3c63246ae91f4
html2334 <bailey.nysen@education.nsw.gov.au>
2025-12-12 02:35:41 +1100

    FINALLY. the buttons log the stop_id when clicked

--- backend/backend.js
+++ backend/backend.js
@@ -1,22 +1,14 @@
-const db = require('better-sqlite3')('foobar.db', {});
-//const db = new sqlite3.Database('db/a.sqlite');
-
-db.prepare("DROP TABLE lorem").run()
-db.prepare("CREATE TABLE lorem (info TEXT)").run()
-
-const stmt = db.prepare("INSERT INTO lorem VALUES (?)")
-for (let i = 0; i < 10; i++) {
-  stmt.run("Ipsum " + i)
-}
-
-/*db.each("SELECT rowid AS id, info FROM lorem", (err, row) => {
-  console.log(row.id + ": " + row.info);
-});*/
-//let log = db.prepare("SELECT rowid AS id, info FROM lorem").all()
-for (const x of db.prepare("SELECT rowid AS id, info FROM lorem").all()) {
-  console.log(x.id + ": " + x.info)
-}
+const db = require('better-sqlite3')('db/data.sqlite', {});
+const express = require('express')
+const app = express()
 
 
-db.prepare("DROP TABLE lorem").run()
-db.close();
+const stmt = db.prepare("SELECT stop_id FROM stations WHERE stop_name = ?")
+
+app.get("/getID", (req, res) => {
+  res.send(stmt.get(req.query.name).stop_id)
+})
+
+app.listen(8081, () => {
+  console.log("Backend active on port 8081")
+})
